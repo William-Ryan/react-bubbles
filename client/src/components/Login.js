baf7dc8677c333658a7a8cca156a450b4aff5da3
William Ryan <billy.ryan.rom@gmail.com>
2020-02-28 11:39:47 -0500

    Add a Login page with a POST Request for Token Access

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,13 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
+import { axiosWithAuth } from "../utils/axiosWithAuth"
+import { useHistory } from "react-router-dom"
 
 const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
+    const [ login, setLogin ] = useState({
+        username: "",
+        password: ""
+    })
+
+    const history = useHistory();
+
+    const handleChange = e => {
+        e.preventDefault();
+        setLogin({
+            ...login, [e.target.name]: e.target.value
+        })
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        axiosWithAuth()
+            .post("login", login)
+            .then(res => {
+                console.log(res)
+                window.localStorage.setItem('token', res.data.payload)
+                history.push("/bubbles")
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
+
   return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
+    <div>
+      <h1>Welcome To the Bubble Show!</h1>
+      <h2>Please Log in</h2>
+      <form onSubmit={handleSubmit}>
+                <label htmlFor="username">Username</label>
+                <input 
+                    type="text"
+                    name="username"
+                    label="username"
+                    value={login.username}
+                    onChange={handleChange}
+                    className="input"
+                />
+                <label htmlFor="password">Password</label>
+                <input 
+                    type="text"
+                    name="password"
+                    label="password"
+                    value={login.password}
+                    onChange={handleChange}
+                    className="input"
+                />
+                <button>Submit</button>
+            </form>
+    </div>
   );
 };
 
